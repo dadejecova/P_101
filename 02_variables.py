2cc263fc26d56f69db15d629fa0bb734569c7f95
Daniel <danielcoellovargas@gmail.com>
2022-11-15 19:21:21 -0500

    Archivos 01_Comentarios y 02_Variables

---
+++ 02_variables.py
@@ -0,0 +1,18 @@
+print("Hola, este es el archivo 01 de python.")
+
+print("Hola soy Daniel y tego 28 años.")
+
+# Operaciones...
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Esto es un comentario
+
+"""
+Varias
+Lineas
+No se pueden ver
+"""
