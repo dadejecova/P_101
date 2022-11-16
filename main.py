f30870472d16dce0c21467183cc5234961ff2c27
Daniel <danielcoellovargas@gmail.com>
2022-11-15 19:12:18 -0500

    Tutorial 01

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hola, esto es python.")
+
+print("Hola soy Daniel y tego 28 años.")
+
+# Operaciones...
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Esto es un comentario
+
+"""
+Varias
+Lineas
+No se pueden ver
+"""
