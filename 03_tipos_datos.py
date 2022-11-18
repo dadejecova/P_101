2875995947a67e80e5ac417396086313a223815e
Daniel <danielcoellovargas@gmail.com>
2022-11-17 19:02:56 -0500

    03_tipos_de_datos

---
+++ 03_tipos_datos.py
@@ -0,0 +1,23 @@
+# String es una cadenas de texto o de carácteres.
+
+my_name1 = "Daniel"
+my_name2 = 'de Jesus'
+
+print("Estos son mis nombres", my_name1, my_name2)
+print(type(my_name1))
+
+# Int son números enteros.
+my_age = 28
+print('Mi edad es', my_age)
+print(type(my_age))
+
+# Boolean, es verdadero o falso.
+is_single = False #True
+print('El valor es:', is_single)
+print(type(is_single))
+
+# inputs
+my_age = input('¿Cúal es tu edad?')
+print("Mi edad es", my_age)
+print(type(my_age))
+# imput siempre será cadena de caracteres.
