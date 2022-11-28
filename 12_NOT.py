554f1c2ff43d2bd2b4ded52bbd0ea8a46cfe73e1
Daniel Coello <danielcoellovargas@gmail.com>
2022-11-28 16:20:44 -0500

    Lógica not

---
+++ 12_NOT.py
@@ -0,0 +1,8 @@
+print(not True)
+print(not False)
+
+# NOT AND
+print('NOT AND')
+print("not True and True =>", not(True and True))
+print("not True and False =>", not(True and False))
+print("not False and False =>", not(False and False))
