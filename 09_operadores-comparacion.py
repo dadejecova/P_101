8a2d8b7eb688eba6d5eb5c54bf630d469bbe6884
Daniel Coello <danielcoellovargas@gmail.com>
2022-11-28 14:11:40 -0500

    Operadores de comparación

---
+++ 09_operadores-comparacion.py
@@ -0,0 +1,35 @@
+# >
+print('Mayor que')
+print(7 > 3)
+print(7 > 7)
+print(3 > 7)
+
+# <
+print('Menor que')
+print(7 < 3)
+print(7 < 7)
+print(3 < 7)
+
+# >=
+print('Mayor o igual que')
+print(7 >= 3)
+print(7 >= 7)
+print(3 >= 7)
+
+# <=
+print('Menor o igual que')
+print(7 <= 3)
+print(7 <= 7)
+print(3 <= 7)
+
+# == extrictamente Igualdad
+print("Igualdad")
+print(6 == 6)
+print(6 == 10)
+print("Apple" == "Apple")
+print("apple" == "Apple")
+
+# != diferencia
+print('Diferencia')
+print(6 != 10)
+print(6 != 6)
