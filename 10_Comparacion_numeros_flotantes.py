021dc9d265adeb00713653100a57c2321418a218
Daniel Coello <danielcoellovargas@gmail.com>
2022-11-28 14:17:32 -0500

    Comparación de numeros flotantes

--- 10_Comparacion_numeros_flotantes.py
+++ 10_Comparacion_numeros_flotantes.py
@@ -3,3 +3,15 @@ print(x)
 y = 1.1 + 2.2
 print(y)
 print(x == y)
+
+y_str = format(y, ".2g")
+print('str =>', y_str)
+#Transformamos a str para que los valores sean iguales
+print(y_str == str(x))
+
+print('+' * 15)
+
+print(y, x)
+
+tolerance = 0.00001
+print(abs(x - y) < tolerance)
