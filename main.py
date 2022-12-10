0eddf7ae81b9a616411df4c82beff60a310f5d64
Daniel Coello <danielcoellovargas@gmail.com>
2022-12-10 16:40:58 -0500

    Main-piedrapapeltijera

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-print("Hola, esto es python.")
+user_option = input('Eliga la opción: Piedra, papel o tijera ->')
+computer_option = 'tijera'
 
-print("Hola soy Daniel y tego 28 años.")
-
-# Operaciones...
-
-print(12 + 5)
-print(10 - 5)
-print(2 * 3)
-print(8 / 2)
-
-# Esto es un comentario
-
-"""
-Varias
-Lineas
-No se pueden ver
-"""
+if user_option == computer_option:
+    print('Empate!')
+elif user_option == 'piedra':
+    if computer_option == 'tijera':
+        print('Piedra le gana a tijera, ganó!')
+    else:
+        print('Papel le gana a piedra, perdiste.')
+elif user_option == 'papel':
+    if computer_option == 'piedra':
+        print('Papel le gana a piedra, ganó!')
+    else:
+        print('Papel no le gana a tijera, perdiste.')
+elif user_option == 'tijera':
+    if computer_option == 'papel':
+        print('tijera le gana a papel, ganó!')
+    else:
+        print('tijera no le gana a piedra, perdiste.')
