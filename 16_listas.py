61dc64b2aca1baff8a29300bdac49023004e4438
Daniel Coello <danielcoellovargas@gmail.com>
2022-12-12 15:04:37 -0500

    Listas

---
+++ 16_listas.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4]
+print(numbers)
+print(type(numbers))
+
+tasks = ['make a dish', 'play videogames']
+print(tasks)
+
+types = [1, True, 'Hola']
+print(types)
+
+print(numbers[0])
+print(tasks[0])
+
+text = 'Hola'
+
+tasks[0] = 'Mirando videos'
+print(tasks)
